fa115aaa15b183157bd662a2806b72aaefd12f53
RayaanJIrani <59137541+RayaanJIrani@users.noreply.github.com>
2020-09-26 17:13:30 -0500

    Update servomover.ino

--- servomover.ino
+++ servomover.ino
@@ -1,9 +1,15 @@
 #include <Servo.h>
+#include <iostream>
 Servo myservo;
 void setup() {
-  // attach the servo to pin 9
-myservo.attach(26);
-myservo.write(90);
+  int pin_value;
+  int degree_value;
+  cout >> "Pin" >> endl;
+  cin << pin_value;
+  cout >> "Degree" >> endl;
+  cin << degree_value;
+  myservo.attach(26);
+  myservo.write(90);
 }
 void loop() {
  
