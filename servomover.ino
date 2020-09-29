4d3a901afc92b3075542f690819f05ac0694753e
RayaanJIrani <59137541+RayaanJIrani@users.noreply.github.com>
2020-09-29 15:49:23 -0500

    Update servomover.ino
    Changed the input pin for information.

--- servomover.ino
+++ servomover.ino
@@ -4,7 +4,7 @@ Servo myservo;
 void setup() {
   int pin_value;
   int degree_value;
-  pin_value = 26;
+  pin_value = 9;
   degree_value = 31;
   myservo.attach(pin_value);
   myservo.write(degree_value);
