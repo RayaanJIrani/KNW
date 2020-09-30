d5e8dceef9d1796c7e27e646685dbd3e516d236f
RayaanJIrani <59137541+RayaanJIrani@users.noreply.github.com>
2020-09-30 14:47:42 -0500

    Update servomover.ino

--- servomover.ino
+++ servomover.ino
@@ -1,25 +1,33 @@
 #include <Servo.h>
-//TODO add user inputs to select pin value.
-Servo myservo;
-void move_motor (int degrees); // Function moves the location n degrees counterclockwise
+
+Servo myservo; // Arduinio IO port hook up 
+void move_continious_motor (int degrees); // Function moves the location n degrees counterclockwise
+double read_photoresistor(int pin); // Function reads the change in resistance caused by the presence of light
+
 void setup() {
-  int pin_value;
-  int degree_value;
-  pin_value = 9;
-  degree_value = 31;
-  myservo.attach(pin_value);
-  move_motor(degree_value);
+  int pin_value = 9;
+  int degree_value = 31;
+  move_motor(degree_value, pin_value);
 }
+
 void loop() {
  
 }  
-void move_motor (int degrees){
-   myservo.write(degrees); // This is a temporary call for the imported code to work 
+
+void move_continious_motor (int degrees = 0, int pin = 9){
+  myservo.attach(pin);
+  myservo.write(degrees); // This is a temporary call for the imported code to work 
   time_start = millis(); // Start time
   const double marginal_run_time = 0; // This is the amount of time longer which each marginal degree causes the program to run
   double end_time = marginal_run_time * degrees + time_start; // This calculates when the program should end 
+  
   while (millis() <= end_time){ // If program should be running it will run
-    myservo.write(180); //move servo as fast as possible
+      myservo.write(180); //move servo as fast as possible
+  }
+  
+  double read_photoresitor(int pin = 9){
+    myservo.attach(pin);
+    return 0.0;
   }
   
 }
