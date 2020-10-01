86425497ac0776d2768c1a591f0d52e83deb9258
RayaanJIrani <59137541+RayaanJIrani@users.noreply.github.com>
2020-09-30 20:15:10 -0500

    Update servomover.ino

--- servomover.ino
+++ servomover.ino
@@ -4,8 +4,8 @@ Servo myservo; // Arduinio IO port hook up
 
 
 
-void move_continious_motor (int degrees); // Function moves the location n degrees counterclockwise
-double read_photoresistor(Photo name); // Function reads the change in resistance caused by the presence of light
+void move_continious_motor (int degrees = 0, int pin = 9); // Function moves the location n degrees counterclockwise
+double read_photoresistor(); // Function reads the change in resistance caused by the presence of light
 
 
 
@@ -19,23 +19,21 @@ void loop() {
  
 }  
 
-void move_continious_motor (int degrees = 0, int pin = 9){
+void move_continious_motor (int degrees, int pin){
   myservo.attach(pin);
   myservo.write(degrees); // This is a temporary call for the imported code to work 
-  time_start = millis(); // Start time
+  int time_start = millis(); // Start time
   const double marginal_run_time = 0; // This is the amount of time longer which each marginal degree causes the program to run
   double end_time = marginal_run_time * degrees + time_start; // This calculates when the program should end 
   
   while (millis() <= end_time){ // If program should be running it will run
       myservo.write(180); //move servo as fast as possible
   }
+}
   
-  int read_photoresitor(Photo name){
-    int light_level = analogRead(name);
+  int read_photoresitor(){
+    int light_level = analogRead(A5);
     Serial.print(light_level);
     Serial.print("\n");
     return light_level;
-  }
-  
-  
-}
+ }
